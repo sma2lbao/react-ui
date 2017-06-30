d1a37a9f0ac04ae5eccae2fcd61659170898476c
sma2lbao <sma2lbao@qq.com>
2017-07-01 00:27:45 +0800

    add select option

--- src/component/input-select/GB-option.jsx
+++ src/component/input-select/GB-option.jsx
@@ -1,22 +1,34 @@
 import React, { Component, PropTypes } from 'react'
-import './GB-option.css'
+import styles from './GB-option.css'
+import cns from 'classnames/bind'
+let cx = cns.bind(styles)
 
 export default class GB_option extends Component {
   static defaultProps = {
-
+    selected: false
   }
 
   static propTypes = {
+    text: PropTypes.string,
+    selected: PropTypes.bool
+  }
 
+  handleClick(event) {
+    if(this.props.onClick) {
+      this.props.onClick(event)
+    }
   }
 
   render() {
     return (
-      <ul>
-        <li>EDIT HTML/CSS</li>
-        <li>EDIT HTML/CSS</li>
-        <li>EDIT HTML/CSS</li>
-      </ul>
+      <li
+        className={cx(styles.li)}
+        onClick={this.handleClick.bind(this)}
+        id={this.props.id}
+        value={this.props.value}
+      >
+        {this.props.text}
+      </li>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React, { Compoent } from 'react'
 import ReactDOM from 'react-dom'
 // import App from './App.jsx'
 // import App from '@/Component/product-button/GB-Prod-button.jsx'
-// import App from './component/button/GB-button.jsx'
-import App from './component/dropdown-button/GB-Dropdown-button.jsx'
+import App from './component/button/GB-button.jsx'
+// import App from './component/dropdown-button/GB-Dropdown-button.jsx'
 // import App from './component/input-select/GB-select.jsx'
 // import Option from './component/input-select/GB-option.jsx'
 
 ReactDOM.render(<App
-  color="blue"
-  text="Hello World"
-  arry={['otption1', 'otption1']}
-  disabled={true}
-  onClick={() => console.log('click')}
-/>,
+  onChange={(value) => console.log(value)}
+  >
+    <Option text="ddd" id="1" />
+    <Option text="ddd" id="1" />
+    <Option text="ddd" id="1" />
+    <Option text="ddd" id="1" />
+    <Option text="ddd" id="1" />
+    <Option text="ddd" id="1" />
+    <Option text="ddd" id="1" />
+    <Option text="ddd" id="1" />
+    <Option text="ddd" id="1" />
+  </App>
+  ,
  document.getElementById('root'));
